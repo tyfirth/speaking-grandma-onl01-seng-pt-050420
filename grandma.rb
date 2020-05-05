1d8cf045da8e7df50a70d7223fb62561c930456f
Tyler Firth <tyfirth@gmail.com>
2020-05-05 20:24:33 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,9 +14,9 @@
 
 def speak_to_grandma(phrase)
   if phrase == phrase.upcase
-    return "NO, NOT SINCE 1938!"
+    puts "NO, NOT SINCE 1938!"
     elsif phrase === "I LOVE YOU GRANDMA!"
-    return "I LOVE YOU TOO PUMPKIN!"
-  else return "HUH?! SPEAK UP, SONNY!"
+    puts "I LOVE YOU TOO PUMPKIN!"
+  else puts "HUH?! SPEAK UP, SONNY!"
  end
 end
